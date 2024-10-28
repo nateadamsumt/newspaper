c7435d277fdabf594d972a0b11260bd97f3b40f0
Nate Adams <nate.adams@umontana.edu>
2024-10-28 17:02:25 -0600

    Chapter 15 completion

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
     list_display = [
         "title",
         "body",
@@ -9,3 +16,4 @@ class ArticleAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
