9c03e705f79fe0bd41a8d8b5b20ff125dbc222f6
Nate Adams <nate.adams@umontana.edu>
2024-10-28 20:58:25 -0600

    seperating added content

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from articles.models import Article
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['article'] = Article.objects.first()
+        return context
